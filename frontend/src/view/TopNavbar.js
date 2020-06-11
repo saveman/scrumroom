a1435612a28d65a4fb7aad338e0863ce932f9cf2
Krzysztof Kapuscik <k.kapuscik@gmail.com>
2020-06-12 00:26:23 +0200

    Navbar layout reworked

--- frontend/src/view/TopNavbar.js
+++ frontend/src/view/TopNavbar.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faUserSlash, faHandPointUp } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faUserSlash, faHandPointUp, faHouseUser } from '@fortawesome/free-solid-svg-icons'
 import { selectUserInfo, logoutUser } from '../model/userInfo';
 import { Link, Switch, Route } from 'react-router-dom';
 
-const TopNavbarUserInfo = () => {
+const TopNavbarUserName = () => {
     const dispatch = useDispatch();
     const userInfo = useSelector(selectUserInfo);
 
@@ -17,7 +17,6 @@ const TopNavbarUserInfo = () => {
     if (userInfo.isLogged) {
         return (
             <div>
-                <Button onClick={onLogout} className="ml-1" size="sm" variant="danger">Logout</Button>
                 <FontAwesomeIcon icon={faUser} className="mx-2" />
                 {userInfo.name}
             </div>
@@ -32,6 +31,38 @@ const TopNavbarUserInfo = () => {
     }
 }
 
+const TopNavbarUserLogout = () => {
+    const dispatch = useDispatch();
+    const userInfo = useSelector(selectUserInfo);
+
+    const onLogout = () => {
+        dispatch(logoutUser(null))
+    };
+
+    if (userInfo.isLogged) {
+        return (
+            <div>
+                <Button onClick={onLogout} className="ml-1" size="sm" variant="danger">Logout</Button>
+            </div>
+        );
+    } else {
+        return <></>
+    }
+}
+
+export const TopNavbarRoomName = ({ match }) => {
+    const getRoomName = (match) => {
+        return match.params.room;
+    }
+
+    return (
+        <Nav.Item>
+            <FontAwesomeIcon icon={faHouseUser} className="mx-2" />
+            {getRoomName(match)}
+        </Nav.Item>
+    );
+};
+
 export const TopNavbarRoomExit = () => {
     return (
         <Link to="/">
@@ -43,17 +74,23 @@ export const TopNavbarRoomExit = () => {
 export const TopNavbar = () => {
     return (
         <Navbar bg="light" expand="lg" sticky="top">
-            <Navbar.Brand className="mr-auto" /*as={Link} to="/"*/>
-                <FontAwesomeIcon icon={faHandPointUp} className="mx-2" />
+            <Navbar.Brand className="mr-auto pr-3" /*as={Link} to="/"*/>
+                <FontAwesomeIcon icon={faHandPointUp} size="2x" className="mx-2" />
                 ScrumRoom
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Switch>
+                        <Route path="/rooms/:room" component={TopNavbarRoomName} />
+                    </Switch>
+                    <TopNavbarUserName />
+                </Nav>
                 <Nav className="ml-auto">
                     <Switch>
                         <Route path="/rooms/:room" component={TopNavbarRoomExit} />
                     </Switch>
-                    <TopNavbarUserInfo />
+                    <TopNavbarUserLogout />
                 </Nav>
             </Navbar.Collapse>
         </Navbar >
